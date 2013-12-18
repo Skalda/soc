593f20d52857ec58a709ee7b6d553c519ad1506c
salicluk <lazzo@seznam.cz>
2013-12-18 04:19:38 +0100

    db

--- app/dbstruktura/_sociocars.sql
+++ app/dbstruktura/_sociocars.sql
@@ -19,6 +19,11 @@ CREATE TABLE `comments` (
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`wall_id`) REFERENCES `wall` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `comments` (`id`, `user_id`, `wall_id`, `date`, `content`) VALUES
+(1,	3,	1,	'2013-12-06 04:19:18',	'ssssssss'),
+(2,	2,	5,	'2013-12-11 00:43:31',	'q'),
+(3,	2,	5,	'2013-12-11 00:43:38',	'q'),
+(4,	2,	5,	'2013-12-11 00:43:42',	'q');
 
 DROP TABLE IF EXISTS `friends`;
 CREATE TABLE `friends` (
@@ -47,6 +52,48 @@ CREATE TABLE `notifications` (
   CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `notifications` (`id`, `users_id`, `time`, `link`, `link_params`, `message`, `attr`, `seen`) VALUES
+(1,	2,	'2013-12-06 02:45:04',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	1),
+(2,	2,	'2013-12-06 02:45:07',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	1),
+(3,	2,	'2013-12-06 02:45:08',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	1),
+(4,	1,	'2013-12-11 01:07:03',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
+(5,	1,	'2013-12-11 01:07:06',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
+(6,	1,	'2013-12-11 01:07:29',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
+(7,	1,	'2013-12-11 01:07:46',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
+(8,	1,	'2013-12-11 01:08:22',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
+(9,	1,	'2013-12-11 01:08:29',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'j',	'[]',	0),
+(10,	1,	'2013-12-11 01:08:42',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'j',	'[]',	0),
+(11,	1,	'2013-12-11 01:08:46',	's',	'{\"id\":5,\"name\":\"stranka\"}',	'j',	'[]',	0);
+
+DROP TABLE IF EXISTS `routes`;
+CREATE TABLE `routes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vehicles_id` int(11) DEFAULT NULL,
+  `name` int(11) DEFAULT NULL,
+  `unit_id` int(11) DEFAULT NULL,
+  `secret_key` int(11) DEFAULT NULL,
+  `measured` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `uploaded` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `length` int(11) DEFAULT NULL,
+  `duration` time DEFAULT NULL,
+  `start_pos` point DEFAULT NULL,
+  `end_pos` point DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vehicles_id` (`vehicles_id`),
+  CONSTRAINT `routes_ibfk_2` FOREIGN KEY (`vehicles_id`) REFERENCES `vehicles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `routeUser`;
+CREATE TABLE `routeUser` (
+  `routes_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  KEY `routes_id` (`routes_id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `routeUser_ibfk_1` FOREIGN KEY (`routes_id`) REFERENCES `routes` (`id`),
+  CONSTRAINT `routeUser_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -64,6 +111,58 @@ CREATE TABLE `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `users` (`id`, `email`, `password`, `salt`, `name`, `surname`, `sex`, `city`, `profilpic`, `filled_data`) VALUES
+(1,	'a@a.cz',	'a',	'',	NULL,	NULL,	'male',	NULL,	NULL,	0),
+(2,	'User1@user.cz',	'b00de2e55ea82ad9fc322e61db74a9720f3b80dd',	'da39a3ee5e6b4b0d3255bfef95601890afd80709',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
+(3,	'User2@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
+(4,	'User3@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
+(5,	'User4@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
+(6,	'User5@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
+(7,	'User6@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0);
+
+DROP TABLE IF EXISTS `vehicles`;
+CREATE TABLE `vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `profilpic` varchar(255) DEFAULT NULL,
+  `info` longtext,
+  `registration_number` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `production_year` int(4) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `type` enum('ground','water','air') DEFAULT NULL,
+  `motor_count` int(11) DEFAULT NULL,
+  `wheel_count` int(11) DEFAULT NULL,
+  `propeller_count` int(11) DEFAULT NULL,
+  `seats_count` int(11) DEFAULT NULL,
+  `window_count` int(11) DEFAULT NULL,
+  `fuel_type` varchar(255) DEFAULT NULL,
+  `engine_capacity` varchar(255) DEFAULT NULL,
+  `engine_power` varchar(255) DEFAULT NULL,
+  `mileage` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `vehicles_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `vehicles` (`id`, `users_id`, `name`, `profilpic`, `info`, `registration_number`, `brand`, `model`, `production_year`, `color`, `type`, `motor_count`, `wheel_count`, `propeller_count`, `seats_count`, `window_count`, `fuel_type`, `engine_capacity`, `engine_power`, `mileage`, `status`) VALUES
+(1,	2,	'addVehicleForm',	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+(2,	2,	'auto',	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
+(3,	2,	'sss',	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL);
+
+DROP TABLE IF EXISTS `vehicleUser`;
+CREATE TABLE `vehicleUser` (
+  `vehicles_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  KEY `vehicles_id` (`vehicles_id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `vehicleUser_ibfk_1` FOREIGN KEY (`vehicles_id`) REFERENCES `vehicles` (`id`),
+  CONSTRAINT `vehicleUser_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `wall`;
 CREATE TABLE `wall` (
@@ -77,5 +176,11 @@ CREATE TABLE `wall` (
   CONSTRAINT `wall_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `wall` (`id`, `user_id`, `date`, `content`, `privacy`) VALUES
+(1,	3,	'2013-12-06 04:19:13',	'sddsd',	0),
+(2,	2,	'2013-12-10 21:16:35',	'sss',	0),
+(3,	2,	'2013-12-11 00:18:30',	'sa',	0),
+(4,	2,	'2013-12-11 00:18:40',	'c',	1),
+(5,	2,	'2013-12-11 00:18:45',	'c',	0);
 
--- 2013-12-04 11:47:18
+-- 2013-12-11 11:41:25
--- app/dbstruktura/sociocars.sql
+++ app/dbstruktura/sociocars.sql
@@ -19,11 +19,6 @@ CREATE TABLE `comments` (
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`wall_id`) REFERENCES `wall` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `comments` (`id`, `user_id`, `wall_id`, `date`, `content`) VALUES
-(1,	3,	1,	'2013-12-06 04:19:18',	'ssssssss'),
-(2,	2,	5,	'2013-12-11 00:43:31',	'q'),
-(3,	2,	5,	'2013-12-11 00:43:38',	'q'),
-(4,	2,	5,	'2013-12-11 00:43:42',	'q');
 
 DROP TABLE IF EXISTS `friends`;
 CREATE TABLE `friends` (
@@ -52,23 +47,12 @@ CREATE TABLE `notifications` (
   CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `notifications` (`id`, `users_id`, `time`, `link`, `link_params`, `message`, `attr`, `seen`) VALUES
-(1,	2,	'2013-12-06 02:45:04',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	1),
-(2,	2,	'2013-12-06 02:45:07',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	1),
-(3,	2,	'2013-12-06 02:45:08',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	1),
-(4,	1,	'2013-12-11 01:07:03',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
-(5,	1,	'2013-12-11 01:07:06',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
-(6,	1,	'2013-12-11 01:07:29',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
-(7,	1,	'2013-12-11 01:07:46',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
-(8,	1,	'2013-12-11 01:08:22',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'',	'[]',	0),
-(9,	1,	'2013-12-11 01:08:29',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'j',	'[]',	0),
-(10,	1,	'2013-12-11 01:08:42',	'',	'{\"id\":5,\"name\":\"stranka\"}',	'j',	'[]',	0),
-(11,	1,	'2013-12-11 01:08:46',	's',	'{\"id\":5,\"name\":\"stranka\"}',	'j',	'[]',	0);
 
 DROP TABLE IF EXISTS `routes`;
 CREATE TABLE `routes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `vehicles_id` int(11) DEFAULT NULL,
+  `vehicles_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
   `name` int(11) DEFAULT NULL,
   `unit_id` int(11) DEFAULT NULL,
   `secret_key` int(11) DEFAULT NULL,
@@ -80,7 +64,9 @@ CREATE TABLE `routes` (
   `end_pos` point DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `vehicles_id` (`vehicles_id`),
-  CONSTRAINT `routes_ibfk_2` FOREIGN KEY (`vehicles_id`) REFERENCES `vehicles` (`id`)
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `routes_ibfk_1` FOREIGN KEY (`vehicles_id`) REFERENCES `vehicles` (`id`),
+  CONSTRAINT `routes_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -114,7 +100,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `email`, `password`, `salt`, `name`, `surname`, `sex`, `city`, `profilpic`, `filled_data`) VALUES
 (1,	'a@a.cz',	'a',	'',	NULL,	NULL,	'male',	NULL,	NULL,	0),
 (2,	'User1@user.cz',	'b00de2e55ea82ad9fc322e61db74a9720f3b80dd',	'da39a3ee5e6b4b0d3255bfef95601890afd80709',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
-(3,	'User2@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
+(3,	'User2@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	'3-SNFM.jpg',	0),
 (4,	'User3@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
 (5,	'User4@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
 (6,	'User5@user.cz',	'8fb688d7dfe7a4ed9d66b8a1aa91208102a27fb8',	'',	NULL,	NULL,	NULL,	NULL,	NULL,	0),
@@ -127,31 +113,23 @@ CREATE TABLE `vehicles` (
   `name` varchar(255) NOT NULL,
   `profilpic` varchar(255) DEFAULT NULL,
   `info` longtext,
-  `registration_number` varchar(255) DEFAULT NULL,
-  `brand` varchar(255) DEFAULT NULL,
-  `model` varchar(255) DEFAULT NULL,
-  `production_year` int(4) DEFAULT NULL,
-  `color` varchar(255) DEFAULT NULL,
-  `type` enum('ground','water','air') DEFAULT NULL,
-  `motor_count` int(11) DEFAULT NULL,
-  `wheel_count` int(11) DEFAULT NULL,
-  `propeller_count` int(11) DEFAULT NULL,
-  `seats_count` int(11) DEFAULT NULL,
-  `window_count` int(11) DEFAULT NULL,
-  `fuel_type` varchar(255) DEFAULT NULL,
-  `engine_capacity` varchar(255) DEFAULT NULL,
-  `engine_power` varchar(255) DEFAULT NULL,
-  `mileage` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `registration_number` varchar(7) DEFAULT NULL,
+  `type` enum('car','motorcycle','quad','tricycle','scooter','motor bike','other') NOT NULL,
+  `mileage` int(11) NOT NULL DEFAULT '0',
+  `status` enum('ready','in use','not ready') NOT NULL DEFAULT 'not ready',
   PRIMARY KEY (`id`),
   KEY `users_id` (`users_id`),
   CONSTRAINT `vehicles_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `vehicles` (`id`, `users_id`, `name`, `profilpic`, `info`, `registration_number`, `brand`, `model`, `production_year`, `color`, `type`, `motor_count`, `wheel_count`, `propeller_count`, `seats_count`, `window_count`, `fuel_type`, `engine_capacity`, `engine_power`, `mileage`, `status`) VALUES
-(1,	2,	'addVehicleForm',	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
-(2,	2,	'auto',	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL),
-(3,	2,	'sss',	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL,	NULL);
+INSERT INTO `vehicles` (`id`, `users_id`, `name`, `profilpic`, `info`, `registration_number`, `type`, `mileage`, `status`) VALUES
+(1,	2,	'addVehicleForm',	NULL,	NULL,	NULL,	'car',	0,	'ready'),
+(2,	2,	'auto',	NULL,	NULL,	NULL,	'car',	0,	'ready'),
+(3,	2,	'sss',	'-SNFM.jpg',	NULL,	NULL,	'car',	0,	'ready'),
+(4,	2,	'dalsi',	NULL,	NULL,	NULL,	'car',	0,	'ready'),
+(5,	2,	'ftyft',	NULL,	NULL,	NULL,	'car',	0,	'ready'),
+(6,	3,	'hgjhg',	NULL,	NULL,	NULL,	'car',	0,	'ready'),
+(7,	2,	'dalsi',	NULL,	NULL,	NULL,	'car',	0,	'ready');
 
 DROP TABLE IF EXISTS `vehicleUser`;
 CREATE TABLE `vehicleUser` (
@@ -183,4 +161,4 @@ INSERT INTO `wall` (`id`, `user_id`, `date`, `content`, `privacy`) VALUES
 (4,	2,	'2013-12-11 00:18:40',	'c',	1),
 (5,	2,	'2013-12-11 00:18:45',	'c',	0);
 
--- 2013-12-11 11:41:25
+-- 2013-12-18 04:18:20
